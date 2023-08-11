2bc05c4e64e4d869c23908fe09c1ce962825daed
Jacobo149 <jtp323@lehigh.edu>
2023-08-10 22:27:06 -0400

    Bulk of frontend added

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  return (
+    <div>
+//To test the app, make sure to run port 5000 first on server side then run the proxy server on port 3000
+    </div>
+  )
+}
+
+export default App
