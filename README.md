12852006909782fa89a2eb3e7b9aeeee8d31959d
Jacobo149 <jtp323@lehigh.edu>
2023-11-19 12:48:27 -0500

    fixed path

---
+++ README.md
@@ -0,0 +1,2 @@
+# Loan_Approval
+Running an ML algorithm on the backend on Loan Approval dataset, will tell you if you are eligble for a loan.
--- server/server.py
+++ server/server.py
@@ -1,30 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-#Change this to __init__ to other machines can easily run the server
-import sys
-#Adding the path to the sys path
-sys.path.insert(0, r"\Users\jakep\OneDrive\Desktop\Projects\Loan_Approval\backend\sources")
-sys.path.insert(0, r"\Users\jakep\OneDrive\Desktop\Projects\Loan_Approval\backend\resources")
-
-
+import os
 from runrf import train, predict
 
+# Get the directory of this script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
+# Add the necessary relative paths to sys.path
+sources_path = os.path.join(script_dir, "backend", "sources")
+resources_path = os.path.join(script_dir, "backend", "resources")
 
+sys.path.insert(0, sources_path)
+sys.path.insert(0, resources_path)
 
 # app instance
 app = Flask(__name__)
 CORS(app)
 
-#/api/home
+# /api/home
 @app.route("/api/home", methods=['POST', 'GET'])
 def return_home():
     if request.method == 'POST':
-
-
         df, randforest = train()
         pred = (request.form['name']).split(',')
-    #[76,0,12,360,1,1,0,1,0,0,0,1,0,0,0,1]
         result = predict(df, randforest, pred)
         return jsonify({"result": result[0]})
 
